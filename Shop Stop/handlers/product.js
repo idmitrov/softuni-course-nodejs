75049580eccacb068bed781480805aaac475895c
idmitrov <mailto.dimitrov@gmail.com>
2017-05-25 01:00:59 +0300

    Implement saving images on the server #lab 2

--- Shop Stop/handlers/product.js
+++ Shop Stop/handlers/product.js
@@ -2,6 +2,8 @@ const url = require('url');
 const fs = require('fs');
 const path = require('path');
 const qs = require('querystring');
+const multiparty = require('multiparty');
+const shortid = require('shortid');
 const utils = require('../utils');
 
 const database = require('../config/database');
@@ -32,15 +34,51 @@ module.exports = (req, res) => {
             res.end();
         });
     } else if (isProductUrl && req.method === 'POST') {
-        let dataString = '';
+        let form = new multiparty.Form()
+            product = {};
 
-        req.on('data', data => {
-            dataString += data;
+        form.on('part', part => {
+            if (part.filename) {
+                let dataString = '';
+
+                part.setEncoding('binary');
+
+                part.on('data', data => {
+                    dataString += data;
+                });    
+
+                part.on('end', () => {
+                    let filename = shortid.generate(),
+                        fileNameParts = part.filename.split(/[.]/g),
+                        fileExtension = fileNameParts[fileNameParts.length - 1];
+                    
+                    filePath = `/content/images/${filename}.${fileExtension}`;
+                    product.image = filePath;
+
+                    fs.writeFile(`.${filePath}`, dataString, {encoding: 'ascii'}, err => {
+                        if (err) {
+                            console.log(err);
+                        } else {
+                            console.log('image saved!')
+                        }
+                    });
+                });
+            } else {
+                let field = '';
+
+                part.setEncoding('utf-8');
+
+                part.on('data', data => {
+                    field += data;
+                });
+
+                part.on('end', () => {
+                    product[part.name] = field;
+                });
+            }
         });
 
-        req.on('end', () => {
-            let product = qs.parse(dataString);
-
+        form.on('close', () => {
             database.products.add(product);
 
             res.writeHead(302, {
@@ -49,6 +87,8 @@ module.exports = (req, res) => {
 
             res.end();
         });
+
+        form.parse(req);
     } else {
         return true;
     }
